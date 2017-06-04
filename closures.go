2cb9eeba0d267cc7eddcaae91ec011a01578601a
Zhu Fangfang <zffforgithub@gmail.com>
2017-06-04 18:29:27 +0800

    add new 1

--- closures.go
+++ closures.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+)
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+
+	fmt.Print(nextInt())
+	fmt.Print(nextInt())
+	fmt.Print(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+}
